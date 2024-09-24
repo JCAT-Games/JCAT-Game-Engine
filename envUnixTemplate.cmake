b5184ed1b2bef4059b929647b6324f3d01503c9e
CMarink <christianm7765@gmail.com>
2024-09-24 17:02:37 -0400

    Completed Initial Makefile for the JCATengine
    The initial makefile for the JCATengine is completed and is working properly.
    
    This makefile successfully links all external required libraries and compiles all source files.
    
    Shader compilation will need to be implemented in the future.

---
+++ envUnixTemplate.cmake
@@ -0,0 +1,7 @@
+# Use the template to create your .env.cmake file, which you will use to find the dependencies needed for this project on your specific device.
+# This is for users on Unix (Linux/macOS)
+
+# Replace the paths with the actual path to your external libraries
+set(GLFW_PATH "/path/to/your/GLFW/version/folder/for/example/glfw-3.4.bin.UNIX")
+set(GLM_PATH "/path/to/your/GLM/folder/named/glm")
+set(VULKAN_SDK_PATH "/path/to/your/Vulkan/version/folder/for/example/1.3.290.0")
---
+++ unixBuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+
+cmake -S ../ -B .
+make && ./JCATengine
+
+cd ..
