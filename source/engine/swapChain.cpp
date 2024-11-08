0731c9a2523ab146c145913fa56fa2adf70dc8c0
CMarink <christianm7765@gmail.com>
2024-11-08 17:08:55 -0500

    Finished Pre-Creation Helper Functions
    Completed the pre-creation helper functions for creating the swap chain.
    
    This includes the functions for choosing the refresh rate, the color format, and the extent of the swap chain.

--- source/engine/swapChain.cpp
+++ source/engine/swapChain.cpp
@@ -1,5 +1,10 @@
 #include "./engine/swapChain.h"
 
+#include <limits>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+
 namespace JCAT {
     SwapChain::SwapChain(DeviceSetup& d, VkExtent2D wE, std::string& gameType, bool v) 
     : device{ d }, windowExtent{ wE } {
@@ -38,7 +43,7 @@ namespace JCAT {
     }
 
     VkResult SwapChain::acquireNextImage(uint32_t* imageIndex) {
-        
+
     }
 
     VkResult SwapChain::submitSwapChainCommandBuffers(const VkCommandBuffer* buffers, uint32_t* imageIndex) {
@@ -51,8 +56,6 @@ namespace JCAT {
         VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
         VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
         VkExtent2D swapExtent = chooseSwapExtent(swapChainSupport.capabilities);
-
-
     }
 
     void SwapChain::createImageViews() {
@@ -76,14 +79,59 @@ namespace JCAT {
     }
 
     VkSurfaceFormatKHR SwapChain::chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+        for (const VkSurfaceFormatKHR& availableFormat : availableFormats) {
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
+                availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+                return availableFormat;
+            }
+        }
 
+        return availableFormats[0];
     }
 
     VkPresentModeKHR SwapChain::chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
+        if (vsyncEnabled) {
+            std::cout << "Refresh Mode: V-Sync" << std::endl;
 
+            for (const VkPresentModeKHR& availiblePresentMode : availablePresentModes) {
+                if (availiblePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+                    std::cout << "Present Mode: Mailbox" << std::endl;
+                    return availiblePresentMode;
+                }
+            }
+
+            std::cout << "Present Mode: FIFO" << std::endl;
+            return VK_PRESENT_MODE_FIFO_KHR;
+        }
+        else {
+            for (const VkPresentModeKHR& availiblePresentMode : availablePresentModes) {
+                if (availiblePresentMode == VK_PRESENT_MODE_IMMEDIATE_KHR) {
+                    std::cout << "Refresh Mode: Immediate" << std::endl;
+                    std::cout << "Present Mode: Immediate" << std::endl;
+                    return availiblePresentMode;
+                }
+            }
+
+            std::cout << "Immediate refresh not availible!" << std::endl;
+            std::cout << "Refresh Mode: V-Sync" << std::endl;
+            std::cout << "Present Mode: FIFO" << std::endl;
+            return VK_PRESENT_MODE_FIFO_KHR;
+        }
     }
 
     VkExtent2D SwapChain::chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+        if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+            return capabilities.currentExtent;
+        }
+        else {
+            VkExtent2D actualExtent = windowExtent;
 
+            actualExtent.width = std::max(capabilities.minImageExtent.width,
+                std::min(capabilities.maxImageExtent.width, actualExtent.width));
+            actualExtent.height = std::max(capabilities.minImageExtent.height,
+                std::min(capabilities.maxImageExtent.height, actualExtent.height));
+
+            return actualExtent;
+        }
     }
 }
