4b2503212ea13dd10e884e5a17dcf2f387b3b8e7
CMarink <christianm7765@gmail.com>
2024-11-28 10:46:22 -0500

    Set Up Header File for renderer
    Completed header file for renderer class. This class will be used to manage the swap chain and the rendering of frames for the game engine.

--- source/engine/renderer.h
+++ source/engine/renderer.h
@@ -2,13 +2,47 @@
 #define RENDERER_H
 
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <cassert>
 
+#include "./engine/window.h"
+#include "./engine/deviceSetup.h"
+#include "./engine/resourceManager.h"
 #include "./engine/swapChain.h"
 
 namespace JCAT {
     class Renderer {
         public:
+            Renderer(Window& w, DeviceSetup& d, ResourceManager& r, std::string type, bool vsync);
+            ~Renderer();
+
+            Renderer(const Renderer&) = delete;
+            Renderer& operator=(const Renderer&) = delete;
+
+            bool isFrameInProgress();
+
+            VkCommandBuffer beginFrame();
+            void endFrame();
+
+            void beginSwapChainRenderPass(VkCommandBuffer commandBuffer);
+            void endSwapChainRenderPass(VkCommandBuffer commandBuffer);
         private:
+            void createCommandBuffers();
+            void freeCommandBuffers();
+            void recreateSwapChain();
+
+            std::string gameType;
+            bool vsync;
+
+            Window& window;
+            DeviceSetup& device;
+            ResourceManager& resourceManager;
+
+            std::unique_ptr<SwapChain> swapChain;
+            std::vector<VkCommandBuffer> commandBuffer;
+
+            bool isFrameStarted;
     };
 }
 
