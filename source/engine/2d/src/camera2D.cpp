45570f997220dcbac318131b85fa5c6d91f727e8
CMarink <christianm7765@gmail.com>
2024-12-01 20:34:42 -0500

    Finished Rendering system for 2D application
    Finished rendering class for the 2D application.
    
    Adjusted Camera class to make sure that it works correctly.
    
    This camera will NOT have scaling and rotation. It will simply just be able to move around.

--- source/engine/2d/src/camera2D.cpp
+++ source/engine/2d/src/camera2D.cpp
@@ -7,29 +7,26 @@ namespace JCAT {
     Camera2D::Camera2D() {}
     Camera2D::~Camera2D() {}
 
-    void Camera2D::setOrthographicProjection(float left, float right, float top, float bottom, float near, float far) {
-        projectionMatrix = glm::mat4{ 1.0f };
+    void Camera2D::setOrthographicProjection(float left, float right, float top, float bottom) {
+        projectionMatrix = glm::mat3(1.0f);
         projectionMatrix[0][0] = 2.f / (right - left);
-        projectionMatrix[1][1] = 2.f / (bottom - top);
-        projectionMatrix[2][2] = 1.f / (far - near);
-        projectionMatrix[3][0] = -(right + left) / (right - left);
-        projectionMatrix[3][1] = -(bottom + top) / (bottom - top);
-        projectionMatrix[3][2] = -near / (far - near);
+        projectionMatrix[1][1] = 2.f / (top - bottom);
+        projectionMatrix[2][0] = -(right + left) / (right - left);
+        projectionMatrix[2][1] = -(top + bottom) / (top - bottom);
     }
 
     void Camera2D::setViewTarget(glm::vec2 position) {
-        viewMatrix = glm::mat4{1.0f};
+        viewMatrix = glm::mat3{1.0f};
 
-        // Set translation (negative position since we move the world relative to the camera)
-        viewMatrix[3][0] = -position.x;
-        viewMatrix[3][1] = -position.y;
+        viewMatrix[2][0] = -position.x;
+        viewMatrix[2][1] = -position.y;
     }
 
-    const glm::mat4& Camera2D::getProjection() const {
+    const glm::mat3& Camera2D::getProjection() const {
         return projectionMatrix;
     }
 
-    const glm::mat4& Camera2D::getView() const {
+    const glm::mat3& Camera2D::getView() const {
         return viewMatrix;
     }
 };
