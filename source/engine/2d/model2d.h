de6b0d4a5fd4ca39f1ff9de5514181ce71b9c4e9
CMarink <christianm7765@gmail.com>
2024-12-10 22:44:04 -0500

    Added Files back

---
+++ source/engine/2d/model2d.h
@@ -0,0 +1,44 @@
+#ifndef MODEL_TWO_D_H
+#define MODEL_TWO_D_H
+
+#include <vector>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm.hpp>
+
+#include "./engine/deviceSetup.h"
+#include "./engine/resourceManager.h"
+
+namespace JCAT {
+    class JCATModel2D {
+        public:
+            struct Vertex2D {
+                glm::vec2 position;
+                glm::vec3 color;
+
+                static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+                static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+            };
+
+            JCATModel2D(DeviceSetup& d, ResourceManager& r, const std::vector<Vertex2D>& spriteVertices);
+            ~JCATModel2D();
+
+            JCATModel2D(const JCATModel2D&) = delete;
+            JCATModel2D& operator=(const JCATModel2D&) = delete;
+
+            void bind(VkCommandBuffer commandBuffer);
+            void draw(VkCommandBuffer commandBuffer);
+
+        private:
+            void createVertexBuffers(const std::vector<Vertex2D>& vertices);
+
+            DeviceSetup& device;
+            ResourceManager& resourceManager;
+            VkBuffer vertexBuffer;
+            VkDeviceMemory vertexBufferMemory;
+            uint32_t vertexCount;
+    };
+};
+
+#endif
