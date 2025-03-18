4b8c7ae61f1f752ba179c8213d0d2b7d9456b549
HenryTheall <thealh@rpi.edu>
2025-03-18 17:24:48 -0400

    Added descriptor sets and pools
    Added header and implementation files for descriptor sets and descriptor pools. Not yet being used anywhere else in the code.

---
+++ source/engine/descriptors.h
@@ -0,0 +1,114 @@
+/*
+ * From Brendan Galea's Vulkan Game Engine Tutorial
+ * https://youtu.be/d5p44idnZLQ?si=jU5LJCSK7UFbBJDT
+ */
+
+#pragma once
+
+#include "./deviceSetup.h"
+
+// std
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+namespace JCAT
+{
+
+    class JCATDescriptorSetLayout
+    {
+    public:
+        class Builder
+        {
+        public:
+            Builder(DeviceSetup &device) : device{device} {}
+
+            Builder &addBinding(
+                uint32_t binding,
+                VkDescriptorType descriptorType,
+                VkShaderStageFlags stageFlags,
+                uint32_t count = 1);
+            std::unique_ptr<JCATDescriptorSetLayout> build() const;
+
+        private:
+            DeviceSetup &device;
+            std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings{};
+        };
+
+        JCATDescriptorSetLayout(
+            LveDevice &lveDevice, std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings);
+        ~JCATDescriptorSetLayout();
+        JCATDescriptorSetLayout(const JCATDescriptorSetLayout &) = delete;
+        JCATDescriptorSetLayout &operator=(const JCATDescriptorSetLayout &) = delete;
+
+        VkDescriptorSetLayout getDescriptorSetLayout() const { return descriptorSetLayout; }
+
+    private:
+        DeviceSetup &device;
+        VkDescriptorSetLayout descriptorSetLayout;
+        std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings;
+
+        friend class JCATDescriptorWriter;
+    };
+
+    class LveDescriptorPool
+    {
+    public:
+        class Builder
+        {
+        public:
+            Builder(DeviceSetup &device) : device{device} {}
+
+            Builder &addPoolSize(VkDescriptorType descriptorType, uint32_t count);
+            Builder &setPoolFlags(VkDescriptorPoolCreateFlags flags);
+            Builder &setMaxSets(uint32_t count);
+            std::unique_ptr<LveDescriptorPool> build() const;
+
+        private:
+            DeviceSetup &device;
+            std::vector<VkDescriptorPoolSize> poolSizes{};
+            uint32_t maxSets = 1000;
+            VkDescriptorPoolCreateFlags poolFlags = 0;
+        };
+
+        JCATDescriptorPool(
+            DeviceSetup &device,
+            uint32_t maxSets,
+            VkDescriptorPoolCreateFlags poolFlags,
+            const std::vector<VkDescriptorPoolSize> &poolSizes);
+        ~LveDescriptorPool();
+        LveDescriptorPool(const LveDescriptorPool &) = delete;
+        LveDescriptorPool &operator=(const LveDescriptorPool &) = delete;
+
+        bool allocateDescriptor(
+            const VkDescriptorSetLayout descriptorSetLayout, VkDescriptorSet &descriptor) const;
+
+        void freeDescriptors(std::vector<VkDescriptorSet> &descriptors) const;
+
+        void resetPool();
+
+    private:
+        DeviceSetup &device;
+        VkDescriptorPool descriptorPool;
+
+        friend class JCATDescriptorWriter;
+    };
+
+    class LveDescriptorWriter
+    {
+    public:
+        JCATDescriptorWriter(JCATDescriptorSetLayout &setLayout, JCATDescriptorPool &pool);
+
+        JCATDescriptorWriter &writeBuffer(uint32_t binding, VkDescriptorBufferInfo *bufferInfo);
+        JCATDescriptorWriter &writeImage(uint32_t binding, VkDescriptorImageInfo *imageInfo);
+
+        bool build(VkDescriptorSet &set);
+        void overwrite(VkDescriptorSet &set);
+
+    private:
+        JCATDescriptorSetLayout &setLayout;
+        JCATDescriptorPool &pool;
+        std::vector<VkWriteDescriptorSet> writes;
+    };
+
+}
